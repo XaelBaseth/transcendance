b42dfccae325a09c5f91c37cb08d4eed8eea7376
Axel Charlot <acharlot@c2r4p4.42lehavre.fr>
2024-04-25 16:00:29 +0200

    update

--- backend/source/settings.py
+++ backend/source/settings.py
@@ -38,7 +38,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '*',
+    'localhost',
 ]
 
 
--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls.static import static
 from django.urls import path
-from . import views
+from .views import testbackend
 from django.conf import settings
 
 urlpatterns = [
-    path('auth/signup', views.signup, name='register'),
-    path('auth/login', views.login, name='register'),
-	path('users/', views.get_users, name='get_users'),
-] + static('/static/', document_root=settings.STATIC_ROOT)
+	path('testbackend/', testbackend, name='testbackend'),
+]
