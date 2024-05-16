703194867a34fae4975b26357e893c12411215a4
Axel Charlot <acharlot@c2r4p4.42lehavre.fr>
2024-05-16 16:29:55 +0200

    update

--- build/frontend/src/components/Navbar/Navbar.tsx
+++ build/frontend/src/components/Navbar/Navbar.tsx
@@ -14,27 +14,27 @@ export default function Navbar(){
 
  return (
 	user &&
-  <>
-    <div className='navbar'>
-		<label className="nav-elements" id="burger-menu" htmlFor="check">
-			<input type="checkbox" id="check"/> 
-			<span className='span1'></span>
-			<span className='span2'></span>
-			<span className='span3'></span> 
-		</label>
-		<div className="navbar__center">
-			<NavLink to="/" className="navbar__title">POKEPONG</NavLink>
+	<>
+		<div className='navbar'>
+			<label className="nav-elements" id="burger-menu" htmlFor="check">
+				<input type="checkbox" id="check"/> 
+				<span className='span1'></span>
+				<span className='span2'></span>
+				<span className='span3'></span> 
+			</label>
+			<div className="navbar__center">
+				<NavLink to="/" className="navbar__title">POKEPONG</NavLink>
+			</div>
+			<>
+				{
+					<div className='nav--avatar'>
+						<Avatar />
+						<button className="logout_btn" onClick={handleLogout}>Logout</button>
+					</div>
+				}
+			</>
 		</div>
-		<>
-			{
-				<div className='nav--avatar'>
-					<Avatar />
-					<button className="logout_btn" onClick={handleLogout}>Logout</button>
-				</div>
- 			}
-		</>
-	</div>
-	<Sidebar />
+		<Sidebar />
   </>
   );
 };
