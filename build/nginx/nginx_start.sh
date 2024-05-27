dff81a2350dae55db137400b577d015b5a6e8c3b
Axel Charlot <acharlot@c2r4p4.42lehavre.fr>
2024-05-27 14:19:13 +0200

    update

--- build/nginx/nginx_start.sh
+++ build/nginx/nginx_start.sh
@@ -2,6 +2,8 @@
 
 if [ ! -f /etc/ssl/certs/nginx.crt ]; then
         openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt -subj "/C=FR/O=42LeHavre/CN=wwww.transcendance.ff";
-fi    
+fi
+
+
 
 exec "$@"
