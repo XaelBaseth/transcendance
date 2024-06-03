3a8c5043966c9d14012eccca6266829ce83cc8c0
Axel Charlot <acharlot@c2r4p4.42lehavre.fr>
2024-06-03 15:31:41 +0200

    update

--- generate_cert.sh
+++ generate_cert.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Debug option
+# Debug option
 set -ex
 
 CERTS_DIR="./ssl/certs"
@@ -8,12 +8,22 @@ PRIVATE_KEY_DIR="./ssl/private"
 
 mkdir -p "$CERTS_DIR" "$PRIVATE_KEY_DIR"
 
-if [ ! -f "${CERTS_DIR}/nginx.crt" ]; then
-    openssl req -x509 -nodes -days 365 -newkey rsa:4096 \
-        -keyout "${PRIVATE_KEY_DIR}/nginx.key" \
-        -out "${CERTS_DIR}/nginx.crt" \
+# Generate the CA certificate
+if [ ! -f "${CERTS_DIR}/ca.crt" ]; then
+    openssl req -new -x509 -days 3650 -keyout "${PRIVATE_KEY_DIR}/ca.key" -out "${CERTS_DIR}/ca.crt" -nodes \
+        -subj "/C=FR/O=42LeHavre/CN=transcendance-ca"
+else
+    echo "CA certificate is already present."
+fi
+
+# Generate the server certificate
+if [ ! -f "${CERTS_DIR}/vault.crt" ]; then
+    openssl req -new -nodes -newkey rsa:4096 -keyout "${PRIVATE_KEY_DIR}/vault.key" -out "${CERTS_DIR}/vault.csr" \
         -subj "/C=FR/O=42LeHavre/CN=www.transcendance.ff" \
         -addext "subjectAltName=DNS:vault,DNS:localhost,DNS:www.transcendance.ff"
+
+    openssl x509 -req -days 365 -in "${CERTS_DIR}/vault.csr" -CA "${CERTS_DIR}/ca.crt" -CAkey "${PRIVATE_KEY_DIR}/ca.key" \
+		 -CAcreateserial -out "${CERTS_DIR}/vault.crt" -extfile <(printf "subjectAltName=DNS:vault,DNS:localhost,DNS:www.transcendance.ff")
 else
-    echo "Certificates are already present."
+    echo "Server certificate is already present."
 fi
--- vault/vault-config.hcl
+++ vault/vault-config.hcl
@@ -1,7 +1,9 @@
 listener "tcp" {
   address     = "0.0.0.0:8200"
-  tls_cert_file = "/etc/vault/certs/nginx.crt"
-  tls_key_file = "/etc/vault/private/nginx.key"
+  tls_disable = 0
+  tls_cert_file = "/etc/vault/certs/vault.crt"
+  tls_key_file = "/etc/vault/private/vault.key"
+  tls_ca_cert_file = "/etc/vault/certs/ca.crt"
 }
 
 storage "file" {
@@ -11,3 +13,4 @@ storage "file" {
 api_addr = "https://127.0.0.1:8200"
 ui = true
 disable_mlock = "true"
+log_requests_level = "trace"
