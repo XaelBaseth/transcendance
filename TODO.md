599717b2282cf86a1dface1e6dbe03ffd5ee0908
Axel Charlot <acharlot@c2r4p4.42lehavre.fr>
2024-06-20 16:02:18 +0200

    Starting

--- TODO.md
+++ TODO.md
@@ -35,6 +35,9 @@ Tentative de setup https, a check
 
 RGPD:
 https://eur-lex.europa.eu/legal-content/FR/TXT/?uri=CELEX:02016R0679-20160504
+Setup the RGPD dans les settings.
+Link vers les settings.
+Installer les dependances necessaires.
 
 
 
--- build/frontend/src/App.tsx
+++ build/frontend/src/App.tsx
@@ -19,6 +19,7 @@ import PongHomePage from "./pages/PongHome"
 import JoinPongRoomPage from "./pages/JoinPongRoomPage"
 import CreatePongRoomPage from "./pages/CreatePongRoomPage"
 import { useTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 
 import './App.css'
 
