5939c3a2587be7e0736a232c592b55c4dcfea076
Robin Luiz <rluiz@c2r6p2.42lehavre.fr>
2024-08-22 16:13:51 +0200

    suppression de quelques prints inutiles dans view.py

--- images/backend/user/views.py
+++ images/backend/user/views.py
@@ -131,17 +131,12 @@ class DeleteAccountView(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 class UpdateProfileView(APIView):
-    print("UpdateProfileView class1")  # Add this line
-    permission_classes = [permissions.AllowAny]
-    print("UpdateProfileView class2")  # Add this line
     def put(self, request):
         user = request.user
-        print(f"User attempting to update profile: {user.username}")  # Add this line
         serializer = UserSerializer(user, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        print(f"Serializer errors: {serializer.errors}")  # Add this line
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class FriendRequestView(APIView):
