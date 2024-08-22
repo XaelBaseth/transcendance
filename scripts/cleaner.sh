6ba9f2be0e4b261ed455f16ac466d31c20a5ab9a
Xael <97090524+XaelBaseth@users.noreply.github.com>
2024-08-22 18:28:48 +0200

    Trying to add ELK
    Elasticsearch working, now needs to be set up to handle every logs

--- scripts/cleaner.sh
+++ scripts/cleaner.sh
@@ -3,6 +3,10 @@
 printf "\033[0;33mStarting the cleaning session!\033[0m\n"
 rm -rf ./volume/vault-data
 sleep 1
+rm -rf ./volume/elasticsearch-data
+sleep 1
+rm -rf ./volume/logs-data
+sleep 1
 docker run --rm -v $(pwd):/app -w /app alpine rm -rf ./volume/db_data
 sleep 1
 rm -rf ./ssl/*
